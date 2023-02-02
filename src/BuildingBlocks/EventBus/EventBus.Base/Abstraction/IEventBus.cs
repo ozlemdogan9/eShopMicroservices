b2dcfde70d08e2b43f74cb0dbc75ab42bd0414ca
ozlemdogan9 <ozzlemdogann@gmail.com>
2023-02-03 00:33:22 +0300

    RabbitMQ and AzureBus implementation.

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventBus.Base.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace EventBus.Base.Abstraction
 {
-    public interface IEventBus
+    public interface IEventBus:IDisposable
     {
+        void Publish(IntegrationEvent @event);
+        void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+        void UnSubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
     }
 }
